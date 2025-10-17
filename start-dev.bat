d706f7e64903ea4b32c655831f1e5775264cb6cc
straus97 <straus97@mail.ru>
2025-10-17 11:36:18 +0300

    feat(setup): настройка Laravel проекта для работы с WAMP
    - Исправлен скрипт start-dev.bat для корректной работы с WAMP
    - Добавлена поддержка UTF-8 кодировки в консоли
    - Настроен файл .env с параметрами подключения к MariaDB
    - Создана база данных turfirma с кодировкой utf8mb4_unicode_ci
    - Выполнены все миграции базы данных
    - Протестирована работа сайта на http://localhost:8000
    
    Все основные компоненты проекта настроены и работают корректно.

--- start-dev.bat
+++ start-dev.bat
@@ -1,4 +1,5 @@
 @echo off
+chcp 65001 >nul
 echo ========================================
 echo   Запуск сервера разработки Авилона
 echo ========================================
@@ -7,7 +8,7 @@ echo.
 echo Проверка зависимостей...
 if not exist "vendor" (
     echo Установка PHP зависимостей...
-    composer install
+    C:\wamp\bin\php\php8.4.13\php.exe C:\wamp\bin\composer\composer.phar install
 )
 
 if not exist "node_modules" (
@@ -16,11 +17,33 @@ if not exist "node_modules" (
 )
 
 echo.
+echo Создание файла .env...
+if not exist ".env" (
+    echo APP_NAME=Авилона > .env
+    echo APP_ENV=local >> .env
+    echo APP_KEY= >> .env
+    echo APP_DEBUG=true >> .env
+    echo APP_URL=http://localhost:8000 >> .env
+    echo. >> .env
+    echo DB_CONNECTION=mysql >> .env
+    echo DB_HOST=127.0.0.1 >> .env
+    echo DB_PORT=3306 >> .env
+    echo DB_DATABASE=turfirma >> .env
+    echo DB_USERNAME=root >> .env
+    echo DB_PASSWORD= >> .env
+    echo Файл .env создан!
+)
+
+echo.
+echo Генерация ключа приложения...
+C:\wamp\bin\php\php8.4.13\php.exe artisan key:generate
+
+echo.
 echo Запуск серверов...
 echo.
 
 echo [1/2] Запуск Laravel сервера на http://localhost:8000
-start "Laravel Server" cmd /k "php artisan serve"
+start "Laravel Server" cmd /k "C:\wamp\bin\php\php8.4.13\php.exe artisan serve"
 
 echo [2/2] Запуск Vite dev server для hot reload
 start "Vite Dev Server" cmd /k "npm run dev"
