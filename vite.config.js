82df46c072cee65bfb8d1d8a39aaa09b486db653
straus97 <straus97@mail.ru>
2026-08-10 21:24:01 +0300

    chore: upgrade vite toolchain

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
     },
     build: {
         outDir: 'public/build',
-        manifest: true,
+        manifest: 'manifest.json',
     },
     publicDir: 'public',
 });
